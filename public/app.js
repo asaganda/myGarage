7f3e4ed2111048351396d3a186847c5325dd6301
Andrew <Asaganda@gmail.com>
2022-11-23 13:37:19 -0500

    created garage door facade look alike on load and click function to slide up open

--- public/app.js
+++ public/app.js
@@ -1,5 +1,18 @@
 $(()=> {
-   $('.hamburger-menu').on('click', () => {
-        $('#nav-links-container').toggle('fast');
-   }) 
+//    $('.hamburger-menu').on('click', () => {
+//         $('#nav-links-container').toggle('fast');
+//    })
+    $('#open-garage-door').on('click', () => {
+        $('#garage-door').css('transform', 'translateY(-100%)')
+    })
+
+    // const makeDivs = () => {
+    //     const divNumber = 4
+    //     const $garageDoor = $('#garage-door');
+    //     for (let i = 0; i < divNumber; i++) {
+    //         const $garageSection = $('<div>').addClass('garage-section')
+    //         $garageDoor.append($garageSection)
+    //     }
+    // }
+    // makeDivs()
 })
