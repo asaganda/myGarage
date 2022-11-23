fa2ce5e6e33af65f28c07377e6aff6b89f99df08
Andrew <Asaganda@gmail.com>
2022-11-23 16:12:51 -0500

    will work on making the garage door work later on, updating edit page

--- public/app.js
+++ public/app.js
@@ -2,9 +2,17 @@ $(()=> {
 //    $('.hamburger-menu').on('click', () => {
 //         $('#nav-links-container').toggle('fast');
 //    })
+    let garageDoorDown = true;
+    // at document ready we want to check garage door boolean value
+
+
     $('#open-garage-door').on('click', () => {
         $('#garage-door').css('transform', 'translateY(-100%)')
+        garageDoorDown = false;
     })
+    // if (garageDoorDown === false) {
+    //     $('#garage-door').css('transform', 'translateY(-100%)')
+    // }
 
     // const makeDivs = () => {
     //     const divNumber = 4
