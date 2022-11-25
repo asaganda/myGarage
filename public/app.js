6f80459512c2eb511579b282788437e8cbc324ea
Andrew <Asaganda@gmail.com>
2022-11-25 17:07:30 -0500

    figured out how to have garage door down only on first page load of /garage using sessionStorage

--- public/app.js
+++ public/app.js
@@ -3,27 +3,11 @@ $(()=> {
 //    $('.hamburger-menu').on('click', () => {
 //         $('#nav-links-container').toggle('fast');
 //    })
-    let garageDoorDown = true;
-    // at document ready we want to check garage door boolean value
-
 
     $('#open-garage-door').on('click', () => {
         $('#garage-door').css('transform', 'translateY(-100%)')
         garageDoorDown = false;
     })
-    // if (garageDoorDown === false) {
-    //     $('#garage-door').css('transform', 'translateY(-100%)')
-    // }
-
-    // const makeDivs = () => {
-    //     const divNumber = 4
-    //     const $garageDoor = $('#garage-door');
-    //     for (let i = 0; i < divNumber; i++) {
-    //         const $garageSection = $('<div>').addClass('garage-section')
-    //         $garageDoor.append($garageSection)
-    //     }
-    // }
-    // makeDivs()
 
     // testing active class with location pathname
     // checking to see if location pathname is /garage/new
@@ -38,4 +22,24 @@ $(()=> {
         }
     }
     activeNavTabCheck()
+
+    // sessionStoreage check
+    // when homepage first loads - garage door shows up, we store some sata in sessionStorage when "open garage" btn is clicked
+    // when homepage loads for anytime after first time and specific location.pathname is hit, we want to check sessionStorage if some data is there (which it should be) , then we don't show it
+    // NOTE: refreshing the page won't reset sessionStorage - we have to kill tab/window
+    $("#open-garage-door").on('click', () => {
+        sessionStorage.setItem("garage", "yes");
+        console.log('garage door opened')
+    })
+
+    const garageDoorCheck = () => {
+        const data = sessionStorage.getItem("garage");
+        console.log(data);
+        if (document.location.pathname === '/garage' && data === "yes") {
+            console.log('i dont want to see garage door')
+            $("#garage-door").css('display', 'none')
+        }
+    }
+
+    garageDoorCheck()
 })
